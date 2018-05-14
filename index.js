a61cf4beecd1472edf8b8857ebd8b18811979e9d
Unknown <rulinshyu@gmail.com>
2018-05-14 21:05:45 +0800

    add x-ray

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const Xray = require('x-ray');
+
+const xray = Xray();
+const courses = xray(
+  'https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html',
+  'table > tr:not(:first-child)',
+  [{
+    number: 'td:nth-child(2)',
+    name: 'td:nth-child(4)',
+    teacher: 'td:nth-child(5)',
+  }]
+);
+
+courses.write('./dist/courses.json');
